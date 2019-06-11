fe32fe9b1c24a750038fb7d0f5d45f2d49ffa598
Subrata Sarker <subrata_sarker@yahoo.com>
2019-06-12 00:31:15 +0900

    Add a general view of the app

--- app/app/urls.py
+++ app/app/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from graphene_django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt
+from files.views import index
 
 urlpatterns = [
+    path('', index, name='index'),
     path('admin/', admin.site.urls),
     path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True)))
 ]
