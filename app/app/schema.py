fad6edf0449b0fcfc8ba82cef45ee93304b6f9cb
Subrata Sarker <subrata_sarker@yahoo.com>
2019-06-12 21:57:57 +0900

    Add JSON web token functionality using django-graphql-jwt

--- app/app/schema.py
+++ app/app/schema.py
@@ -1,4 +1,5 @@
 import graphene
+import graphql_jwt
 import files.schema
 import users.schema
 
@@ -8,7 +9,10 @@ class Query(users.schema.Query, files.schema.Query, graphene.ObjectType):
 
 
 class Mutation(users.schema.Mutation, files.schema.Mutation, graphene.ObjectType):
-    pass
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    verify_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
+
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
