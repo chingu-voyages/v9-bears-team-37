91ae1e56fd2851a627ede288c368ce9ec2c6491b
Subrata Sarker <subrata_sarker@yahoo.com>
2019-06-12 14:35:33 +0900

    Create mutation for file

--- app/files/schema.py
+++ app/files/schema.py
@@ -25,15 +25,23 @@ class CreateFile(graphene.Mutation):
         downloadable_at = graphene.DateTime()
         downloadable_during = graphene.String()
 
-    def mutate(self, info, name, description, 
-            size, url, downloadable_at, 
-            downloadable_during):
-        file = File(name=name, desciption=description, 
-            size=size, url=url, downloadable_at=downloadable_at, 
-            downloadable_during=downloadable_during)
+    def mutate(self, info, **kwargs):
+        '''
+        name = kwargs.get('name')
+        description = kwargs.get('description')
+        size = kwargs.get('size')
+        url = kwargs.get('url')
+        downloadable_at = kwargs.get('downloadable_at')
+        downloadable_during = kwargs.get('downloadable_during')
+        '''
+        # file = File(name=name, desciption=description, 
+        #     size=size, url=url, downloadable_at=downloadable_at, 
+        #     downloadable_during=downloadable_during)
+        file = File(kwargs)
         file.save()
         return CreateFile(file=file)
 
 
-
+class Mutation(graphene.ObjectType):
+    create_file = CreateFile.Field()
 
