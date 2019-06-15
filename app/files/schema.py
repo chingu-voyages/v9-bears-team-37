6ec1ac36336a552df12d3e08baab16af3d16fb7d
Shahzad <hope_charm_angel@yahoo.com>
2019-06-15 16:36:17 +0600

    Please check files/schema and users/schema :D
    Please check files/schema and users/schema

--- app/files/schema.py
+++ app/files/schema.py
@@ -55,6 +55,7 @@ class UpdateFile(graphene.Mutation):
         description = graphene.String()
         size = graphene.String()
         url = graphene.String()
+        # can you check this. DateTime needs option. Without option. and we dont need downloadable_at query
         downloadable_at = graphene.DateTime()
         downloadable_during = graphene.String()
 
