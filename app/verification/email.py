b08fac9398b6d6761c3a3a0f2cbc5b591d40730d
Subrata Sarker <subrata_sarker@yahoo.com>
2019-07-08 11:16:27 +0900

    Use templates to send email

--- app/verification/email.py
+++ app/verification/email.py
@@ -1,13 +1,18 @@
 from django.conf import settings
 from django.shortcuts import render
 from django.core.mail import send_mail
+from django.template.loader import render_to_string
 
 
 def index(request):
+    name = "Picklu"
+    text_message = render_to_string('email/message.txt', {'name': name})
+    html_message = render_to_string('email/message/email.html', {'name': name})
+    
     result = send_mail(
-        subject='Hello from Picklu',
-        message="Hello, Picklu",
-        html_message='<html><body><p>Hello there, This is an <b>automated</b> html email.</p></body></html>',
+        subject="Hello, there",
+        message=text_message,
+        html_message=html_message,
         from_email=settings.EMAIL_HOST_USER,
         recipient_list=['picklumithu@gmail.com'],
         fail_silently=False)
