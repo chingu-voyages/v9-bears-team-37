1baf753c2b869be137f4d2b6100b2f8e029c8283
Subrata Sarker <subrata_sarker@yahoo.com>
2019-07-08 10:27:29 +0900

    Conditionally render success/fail html page

--- app/verification/email.py
+++ app/verification/email.py
@@ -4,11 +4,14 @@ from django.core.mail import send_mail
 
 
 def index(request):
-    send_mail(
+    result = send_mail(
         subject='Hello from Picklu',
         message="Hello, Picklu",
         html_message='<html><body><p>Hello there, This is an <b>automated</b> html email.</p></body></html>',
         from_email=settings.EMAIL_HOST_USER,
         recipient_list=['picklumithu@gmail.com'],
         fail_silently=False)
-    return render(request, 'send/index.html')
+    if result:
+        return render(request, 'send/success.html')
+    else:
+        return render(request, 'send/fail.html')
