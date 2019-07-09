998a11346b951a694e042a24f3d4bced49d57d62
Subrata Sarker <subrata_sarker@yahoo.com>
2019-07-09 14:13:27 +0900

    Send verification email with user id

--- app/verification/email.py
+++ app/verification/email.py
@@ -1,27 +1,22 @@
+import graphene
+import json
 from django.conf import settings
 from django.http import JsonResponse
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.views.decorators.csrf import csrf_exempt
-from gql import gql, Client
-from gql.transport import requests
-import json
+from app.schema import Query
 
 
-query = gql(
-    """query($email: String!){
+query = """
+    query($email: String!){
         user(email: $email) {
             id
             username
             email
             isVerified
         }
-}""")
-
-def run_query(variables):
-    transport = requests.RequestsHTTPTransport(url='http://localhost:8000/graphql/')
-    client = Client(transport=transport)
-    return client.execute(query, variables)
+    }"""
 
 
 @csrf_exempt
@@ -33,39 +28,35 @@ def collect_email(request):
     body = json.loads(body_unicode)
     username = body.get('username', 'there')
     email = body.get('email', '')
-    if not email:
-        JsonResponse({'msg': 'Email id was not found!'})
-    
-    try:
-        variables = {
-            "email": email
-        }
-        response = run_query(variables)
-    except Exception as e:
-        print(e)
-        return JsonResponse({'error': 'Failed to send request to graphql'})
-    
-    if response.status_code == 200:
-        return JsonResponse(response.json())
+
+    schema = graphene.Schema(query=Query)
+    response = schema.execute(query, variables={"email": email})
+
+    data = response.to_dict()['data']
+
+    if data['user']:
+
+        user = data['user']
+        if user['isVerified']:
+            return JsonResponse({'msg': 'User is already verified!'})
+
+        # User is found and the user's email is not verified
+        id = user['id']
+        text_message = render_to_string('email/message.txt', {'name': username, 'id': id})
+        html_message = render_to_string('email/message.html', {'name': username, 'id': id})
+
+        result = send_mail(
+            subject="Email verification for HyperFlashDrive",
+            message=text_message,
+            html_message=html_message,
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[email],
+            fail_silently=False)
+
+        if result:
+            return JsonResponse(data)
     else:
-        return JsonResponse({'msg': 'Something went wrong in the query!', 'status_code': response.status_code})
-
-    # text_message = render_to_string('email/message.txt', {'name': username, 'id': 7})
-    # html_message = render_to_string('email/message.html', {'name': username, 'id': 7})
-
-    # result = send_mail(
-    #     subject="Verify your email",
-    #     message=text_message,
-    #     html_message=html_message,
-    #     from_email=settings.EMAIL_HOST_USER,
-    #     recipient_list=[email],
-    #     fail_silently=False)
-
-    # if result:
-    #     return JsonResponse({
-    #         'name': name, 'email': email, 'msg': 'Email collection was successfull!'
-    #     })
-    # return JsonResponse({'msg': 'Something went wrong!'})
+        return JsonResponse({'msg': 'User not found!'})
 
 
 def confirm_email(request, id):
