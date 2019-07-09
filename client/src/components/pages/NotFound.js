6c8ce906acdaf0d295921f56a34e81519fa2f810
Subrata Sarker <subrata_sarker@yahoo.com>
2019-07-09 21:57:13 +0900

    Remove extra scripts from notfound component

--- client/src/components/pages/NotFound.js
+++ client/src/components/pages/NotFound.js
@@ -1,16 +1,6 @@
 import React from 'react';
-import { Query } from 'react-apollo';
-import { gql } from 'apollo-boost';
-
-import FileList from '../File/FileList';
-import CreateFile from '../File/CreateFile';
-import Loading from '../Common/Loading';
-import ShowError from '../Common/ShowError';
-import { Row, Col } from 'react-bootstrap';
 import { Typography } from '@material-ui/core';
 
-const NotFound = () => {
-  return <Typography variant='h2'>Page Not Found</Typography>;
-};
+const NotFound = () => <Typography variant='h3'>Page Not Found</Typography>;
 
 export default NotFound;
