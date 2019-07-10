ac1e97d093b01dfd580599656c87004ebf85432c
Subrata Sarker <subrata_sarker@yahoo.com>
2019-07-10 22:19:09 +0900

    Update config.js with api urls

--- client/src/components/File/CreateFile.js
+++ client/src/components/File/CreateFile.js
@@ -6,7 +6,7 @@ import { sendEmail } from '../../helpers';
 import { GET_DLFILES_QUERY } from '../pages/Root';
 import ShowError from '../Common/ShowError';
 import { UserContext } from '../../App';
-import { CLOUD_API_URL, EMAIL_API_URL } from '../../config';
+import { CLOUD_API_URL } from '../../config';
 import withStyles from '@material-ui/core/styles/withStyles';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
